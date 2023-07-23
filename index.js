4ab08dd85d52d9cc9fd14bbc9c87f0eedf8169bd
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-23 16:36:31 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards = [];
+    for(let i = 0; i < names.length; i++ ){     
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        cards.push(message)
+    }  
+    return cards;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"] , "surprise"));
+
+
+const numbers = []
+
+function countDown(numbers){
+    let i = 0  ;
+    while(i < 11){
+        console.log(i++)
+    }
+    return numbers;
+}
+
+countDown(numbers);
+
+
+
